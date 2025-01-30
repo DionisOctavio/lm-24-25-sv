6888eb28814b7609ee3922f33fde3ed5cfd720ad
DionisOctavio <dionisoctavioalvarezescorcia@gmail.com>
2025-01-30 14:05:10 +0100

    [30/01/2025] 14:05
    [30/01/2025] 14:05

---
+++ 013 CHALLENGE/05/main.js
@@ -0,0 +1,30 @@
+const API_URL = "http://localhost:3000/peliculas";
+
+const GET_PELICULAS = "/peliculas";
+const GET_PELICULAS_BY_GENNERO = API_URL + "";
+const GET_PELICULAS_FABORITAS = API_URL + "";
+const GET_USUARIOS = API_URL + "";
+
+function getPeliculas(){
+
+    alert("Paso1");
+
+    /*fetch()
+        .then(response => response.json())
+        .then(
+            (data) => {}
+        )
+        .catch();*/
+
+        fetch(GET_PELICULAS)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                let idPelicula = data[0].id
+                let tituloPelicula = data[0].titulo
+            }
+        )
+        .catch();
+
+
+}
